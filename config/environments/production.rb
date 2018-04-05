4ae365d4edf4a72c6ff12cf863dd7f17bad14e61
Gurpreet Singh <gurpreet@vinsol.com>
2018-04-05 11:02:44 +0530

    Minor-fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,7 @@ Hours::Application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = SMTP_SETTINGS
   config.action_mailer.default_url_options = { host: 'vinsol-resource-management.herokuapp.com' }
