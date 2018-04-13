76fce0701ec85f5d3eb88d25194f83309025f5b9
Gurpreet Singh <gurpreet@vinsol.com>
2018-04-13 18:41:05 +0530

    Minor-fix

--- app/services/daily_updates_manager.rb
+++ app/services/daily_updates_manager.rb
@@ -1,7 +1,7 @@
 class DailyUpdatesManager
   def self.bulk_update attributes_collection, user
     attributes_collection.values.map do |attributes|
-      daily_update = user.daily_updates.find_or_initialize_by(date: attributes[:date])
+      daily_update = user.daily_updates.find_or_create_by(date: attributes[:date])
       daily_update.assign_attributes(attributes)
       daily_update.save
     end.all?(&:present?)
