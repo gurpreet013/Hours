daea7070b692fefa3448e13938476a7b373747fe
Gurpreet Singh <gurpreet@vinsol.com>
2018-05-18 00:55:11 +0530

    Project Manager Abilities added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   end
 
   def index
-    @users = User.all
+    @users = User.all.accessible_by(current_ability)
   end
 
   def edit
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -17,6 +17,16 @@ class Ability
     end
 
     def project_manager_abilities
+      can :read, Project, { project_manager_id: current_user.id }
+      can :update, Project do |project|
+        project.project_manager_id == current_user.id
+      end
+      can :all_timecards, DailyUpdate
+      can :read, Report
+      can :read, User, projects: { project_manager_id: current_user.id }
+      can :update, User do |user|
+        user.projects.where(project_manager_id: current_user.id).exists?
+      end
     end
 
     def staff_abilities
@@ -25,7 +35,8 @@ class Ability
       can [:read, :bulk_update], DailyUpdate
       can [:read, :update, :destroy], Hour, daily_update: { user_id: current_user.id }
       can [:read, :destroy], Entry, daily_update: { current_user_id: current_user.id }
-      can [:show, :update, :edit], User do |user|
+      can :index, User
+      can [:show, :update], User do |user|
         current_user.id == user.id
       end
     end
